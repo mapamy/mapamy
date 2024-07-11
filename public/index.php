4c87057f438d7ba536e2900338dceed60ccdfb52
Alvaro Franz <mail@alvarofranz.com>
2024-07-11 16:12:24 +0200

    Remove irrelevant comments

--- public/index.php
+++ public/index.php
@@ -65,13 +65,11 @@ $uri = rtrim($_SERVER['REQUEST_URI'], '/');
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
-        // ... 404 Not Found
         http_response_code(404);
         echo 'Page not found';
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
-        // ... 405 Method Not Allowed
         http_response_code(405);
         echo 'Method not allowed';
         break;
