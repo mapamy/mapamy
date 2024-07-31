b1bfe230fb6fb751c55b2b3c237ba4e1ea84b111
Alvaro Franz <mail@alvarofranz.com>
2024-07-31 13:04:28 +0200

    Add list of random maps to home

--- public/home.php
+++ public/home.php
@@ -1,13 +1,26 @@
 <?php
 
+use App\Database;
+use App\Map;
+
 // Redirect to dashboard if logged in
 if (isset($_SESSION['user_id'])) {
     header('Location: /dashboard');
     exit;
 }
 
+// Get the database connection
+$pdo = (new Database())->getConnection();
+
+// Create a new map instance
+$map = new Map($pdo);
+
+// Get 10 random maps
+$maps = $map->getRandomMaps(10);
+
 $view = [
     'baseUrl' => $_ENV['BASE_URL'],
+    'maps' => $maps,
 ];
 
 // Load the home view
--- views/home.php
+++ views/home.php
@@ -5,15 +5,20 @@ if (!isset($view)) {
 ?>
 <main class="main">
     <h1>Mapamy</h1>
-    <h2>Welcome!</h2>
-    <p>You need to login:</p>
+    <h2>Access</h2>
     <ul>
-        <li><a href='<?php echo $view['baseUrl']; ?>/google-login'>Access with Google</a>: Login using your Google
-            account
+        <li><a href='<?php echo $view['baseUrl']; ?>/google-login'>With Google</a>
         </li>
-        <li><a href='<?php echo $view['baseUrl']; ?>/email-login'>Access with email</a>: Login receiving a magic link
-            via
-            email
-        </li>
+        <li><a href='<?php echo $view['baseUrl']; ?>/email-login'>With email</a></li>
     </ul>
+    <h2>Random maps</h2>
+    <ul>
+        <?php
+        foreach ($view['maps'] as $map) {
+            echo "<li><a href='{$view['baseUrl']}/m/{$map['slug']}'>{$map['name']}</a></li>";
+        }
+        ?>
+    <?php
+    include __DIR__ . '/partials/legal.php';
+    ?>
 </main>
